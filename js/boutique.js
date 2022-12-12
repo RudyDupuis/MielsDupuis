092a03d9b050e059a88d6a5d6f4ce6e1e6961858
RudyDupuis <abcdupuisrudy@gmail.com>
2022-12-12 18:34:41 +0100

    add product in boutique (javascript)

---
+++ js/boutique.js
@@ -0,0 +1,186 @@
+/** Products **/
+
+let colzaKg = {
+    img : 'assets/pictures/boutique/ColzaPot1Kg.png',
+    alt : `Photo d'un pot de miel de Colza`,
+    title: 'Miel de Colza - 1kg',
+    price: '10.00 €',
+    desc : `Miel de colza aussi appellé « miel de printemps » est le premier miel de la saison. Il est composé en grande majorité de fleurs de Colza mais aussi de toutes les autres fleurs de printemps. C’est un miel dur et facile à tartiner. Je suis sûr que vous allez l’adorer !`,
+    caract1 : "<strong>Texture :</strong> Dur",
+    caract2 : "<strong>Goût :</strong> Doux",
+    advice : `<strong>Conservation :</strong><br>La date de durabilité minimal (DDM) est de 2 ans après la mise en pot. Pour une bonne conservation du miel, gardez le pot fermé dans un endroit frais et sombre. `
+}
+let tournesolKg = {
+    img : 'assets/pictures/boutique/TournesolPot1Kg.png',
+    alt : `Photo d'un pot de miel de Tournesol`,
+    title: 'Miel de Tournesol - 1kg',
+    price: '10.00 €',
+    desc : `Miel de tournesol aussi appellé « miel d’été » est le dernier miel de la saison. Il est composé en grande majorité de fleurs de Tournesol mais aussi de toutes les autres fleurs d’été. C’est un miel dur avec un goût léger. Je suis sûr que vous allez l’adorer !`,
+    caract1 : "<strong>Texture :</strong> Dur",
+    caract2 : "<strong>Goût :</strong> Doux",
+    advice : `<strong>Conservation :</strong><br>La date de durabilité minimal (DDM) est de 2 ans après la mise en pot. Pour une bonne conservation du miel, gardez le pot fermé dans un endroit frais et sombre. `
+}
+let fleursKg = {
+    img : 'assets/pictures/boutique/FleursPot1Kg.png',
+    alt : `Photo d'un pot de miel de Fleurs`,
+    title: 'Miel de Fleurs - 1kg',
+    price: '12.00 €',
+    desc : `Miel de Fleurs est généralement récolté fin juin. La troisième récolte de l’année. Il est souvent composé en grande majorité de fleurs de tilleul, de châtaignier et de ronce mais aussi d’autres fleurs. Vu qu’il est composé d’un grand nombre de fleurs, chaque année, sa composition et son goût sont très différents. C’est un miel dur avec un goût corsé. Je suis sûr que vous allez l’adorer !`,
+    caract1 : "<strong>Texture :</strong> Dur",
+    caract2 : "<strong>Goût :</strong> Corsé",
+    advice : `<strong>Conservation :</strong><br>La date de durabilité minimal (DDM) est de 2 ans après la mise en pot. Pour une bonne conservation du miel, gardez le pot fermé dans un endroit frais et sombre. `
+}
+let acaciaKg = {
+    img : 'assets/pictures/boutique/AcaciaPot1Kg.png',
+    alt : `Photo d'un pot de miel d'Acacia`,
+    title: `Miel d'Acacia - 1kg`,
+    price: '16.00 €',
+    desc : `Miel d’Acacia est généralement récolté fin mai. La seconde récolte de l’année. Il est composé en grande majorité de fleurs d’Acacia. C’est un miel qui reste liquide longtemps avec un goût léger. Je suis sûr que vous allez l’adorer !`,
+    caract1 : "<strong>Texture :</strong> liquide",
+    caract2 : "<strong>Goût :</strong> Doux",
+    advice : `<strong>Conservation :</strong><br>La date de durabilité minimal (DDM) est de 2 ans après la mise en pot. Pour une bonne conservation du miel, gardez le pot fermé dans un endroit frais et sombre. `
+}
+let colzaG = {
+    img : 'assets/pictures/boutique/ColzaPot500g.png',
+    alt : `Photo d'un pot de miel de Colza`,
+    title: 'Miel de Colza - 500g',
+    price: '5.50 €',
+    desc : `Miel de colza aussi appellé « miel de printemps » est le premier miel de la saison. Il est composé en grande majorité de fleurs de Colza mais aussi de toutes les autres fleurs de printemps. C’est un miel dur et facile à tartiner. Je suis sûr que vous allez l’adorer !`,
+    caract1 : "<strong>Texture :</strong> Dur",
+    caract2 : "<strong>Goût :</strong> Doux",
+    advice : `<strong>Conservation :</strong><br>La date de durabilité minimal (DDM) est de 2 ans après la mise en pot. Pour une bonne conservation du miel, gardez le pot fermé dans un endroit frais et sombre. `
+}
+let tournesolG = {
+    img : 'assets/pictures/boutique/TournesolPot500g.png',
+    alt : `Photo d'un pot de miel de Tournesol`,
+    title: 'Miel de Tournesol - 500g',
+    desc : `Miel de tournesol aussi appellé « miel d’été » est le dernier miel de la saison. Il est composé en grande majorité de fleurs de Tournesol mais aussi de toutes les autres fleurs d’été. C’est un miel dur avec un goût léger. Je suis sûr que vous allez l’adorer !`,
+    price: '5.50 €',
+    desc : `Miel de tournesol aussi appellé « miel d’été » est le dernier miel de la saison. Il est composé en grande majorité de fleurs de Tournesol mais aussi de toutes les autres fleurs d’été. C’est un miel dur avec un goût léger. Je suis sûr que vous allez l’adorer !`,
+    caract1 : "<strong>Texture :</strong> Dur",
+    caract2 : "<strong>Goût :</strong> Doux",
+    advice : `<strong>Conservation :</strong><br>La date de durabilité minimal (DDM) est de 2 ans après la mise en pot. Pour une bonne conservation du miel, gardez le pot fermé dans un endroit frais et sombre. `
+}
+let fleursG = {
+    img : 'assets/pictures/boutique/FleursPot500g.png',
+    alt : `Photo d'un pot de miel de Fleurs`,
+    title: 'Miel de Fleurs - 500g',
+    price: '6.50 €',
+    desc : `Miel de Fleurs est généralement récolté fin juin. La troisième récolte de l’année. Il est souvent composé en grande majorité de fleurs de tilleul, de châtaignier et de ronce mais aussi d’autres fleurs. Vu qu’il est composé d’un grand nombre de fleurs, chaque année, sa composition et son goût sont très différents. C’est un miel dur avec un goût corsé. Je suis sûr que vous allez l’adorer !`,
+    caract1 : "<strong>Texture :</strong> Dur",
+    caract2 : "<strong>Goût :</strong> Corsé",
+    advice : `<strong>Conservation :</strong><br>La date de durabilité minimal (DDM) est de 2 ans après la mise en pot. Pour une bonne conservation du miel, gardez le pot fermé dans un endroit frais et sombre. `
+}
+let acaciaG = {
+    img : 'assets/pictures/boutique/AcaciaPot500g.png',
+    alt : `Photo d'un pot de miel d'Acacia`,
+    title: `Miel d'Acacia - 500g`,
+    price: '8.50 €',
+    desc : `Miel d’Acacia est généralement récolté fin mai. La seconde récolte de l’année. Il est composé en grande majorité de fleurs d’Acacia. C’est un miel qui reste liquide longtemps avec un goût léger. Je suis sûr que vous allez l’adorer !`,
+    caract1 : "<strong>Texture :</strong> liquide",
+    caract2 : "<strong>Goût :</strong> Doux",
+    advice : `<strong>Conservation :</strong><br>La date de durabilité minimal (DDM) est de 2 ans après la mise en pot. Pour une bonne conservation du miel, gardez le pot fermé dans un endroit frais et sombre. `
+}
+let mielEtNoisettes = {
+    img : 'assets/pictures/boutique/MieletNoisettesPot480g.png',
+    alt : `Photo d'un pot de pâte à tartiner au miel et noisettes`,
+    title: 'Pâte à tartiner au miel et noisettes - 480g',
+    price: '6.50 €',
+    desc : `Préparation au miel de Colza onctueux mélangé avec de la poudre de noisette bio. Il remplacera vos pâtes à tartiner pour des petits déjeuners de qualité !`,
+    caract1 : "<strong>Texture :</strong> Onctueuse",
+    caract2 : "<strong>Goût :</strong> Doux",
+    advice : `<strong>Conservation :</strong><br>Avec l’ajout de noisette la date de durabilité minimal (DDM) est de 6 mois après la mise en pot. Pour une bonne conservation du miel, gardez le pot fermé dans un endroit frais et sombre.  `
+}
+let bougiePot = {
+    img : 'assets/pictures/boutique/BougiePot.png',
+    alt : `Photo d'un pot de bougie aux huiles essentielles`,
+    title: 'Bougie aux huiles essentielles',
+    price: '6.00 €',
+    desc : `Bougie artisanale à la cire d’abeille avec une mèche en coton et aux huiles essentielles bio d’Ylang-Ylang, de Palmarosa et d’Orange douce. <br>Hauteur 8.5 cm | Diamètre 6.5 cm`,
+    caract1 : "<strong>Flamme :</strong> Très grande",
+    caract2 : "<strong>Durée :</strong> ≃ 20h",
+    advice : `<strong>Conseils :</strong><br>Fermer le pot pour l’éteindre évite de faire de la fumée. <br><br><strong>Attention :</strong><br>Ne pas laisser proche d’objet inflammable. Pot en verre très chaud. Huiles essentielles déconseillées aux femmes enceintes. Peut provoquer des allergies. `
+}
+let bougieLongue = {
+    img : 'assets/pictures/boutique/BougieLongue.png',
+    alt : `Photo d'un lot de 5 bougies longues`,
+    title: 'Lot de 5 bougies longues',
+    price: '4.00 €',
+    desc : `Bougies longues à la cire d’abeille avec une mèche en coton. Les bougies sont réalisées à la main une à une. <br>Hauteur ≃ 20 cm | Diamètre ≃ 1.5 cm`,
+    caract1 : "<strong>Flamme :</strong> Très grande",
+    caract2 : "<strong>Durée :</strong> ≃ 4h",
+    advice : `<strong>Attention :</strong><br>Ne pas laisser proche d’objet inflammable. `
+}
+
+const honeyPlace = document.getElementById("miels");
+const candlePlace = document.getElementById("bougies");
+const honeys = [colzaKg, tournesolKg, fleursKg, acaciaKg, colzaG, tournesolG, fleursG, acaciaG, mielEtNoisettes];
+const candles = [bougiePot, bougieLongue];
+
+function createProductCard(amount, place){
+    for (let i = 0; i < amount.length; i++) {
+        place.innerHTML = place.innerHTML + 
+    `<div class="product">
+        <img src="" alt="">
+        <div class="product__description">
+            <h3></h3>
+            <p></p>
+            <div class="product__description-inputs">
+                <input type="button" value="En savoir plus" class="learnMore-button ${place.id} ${i}">
+                <input type="image" src="assets/pictures/header/picto_panier.webp" alt="Dessin d'un panier (ajouter au panier)">
+            </div>
+        </div>
+    </div>`;
+    }
+}
+
+createProductCard(honeys, honeyPlace);
+createProductCard(candles, candlePlace);
+
+const productsData = [colzaKg, tournesolKg, fleursKg, acaciaKg, colzaG, tournesolG, fleursG, acaciaG, mielEtNoisettes, bougiePot, bougieLongue];
+const productsPlace = document.querySelectorAll('.product');
+
+for (let i = 0; i < productsPlace.length; i++) {
+    productsPlace[i].children[0].src = productsData[i].img;
+    productsPlace[i].children[0].alt = productsData[i].alt;
+    productsPlace[i].children[1].children[0].textContent = productsData[i].title;
+    productsPlace[i].children[1].children[1].textContent = productsData[i].price;    
+}
+
+const learnMore = document.querySelector('.learnMore');
+const learnMoreButton = document.querySelectorAll('.learnMore-button');
+const cross = document.querySelector('.learnMore__cross');
+
+cross.addEventListener('click', ()=> {
+    learnMore.style.display = "none";
+})
+
+function createProductPage(category, product) {
+    learnMore.children[1].children[1].src = category[product].img;
+    learnMore.children[1].children[1].alt = category[product].alt;
+    learnMore.children[1].children[0].textContent = category[product].title;
+    learnMore.children[2].children[0].children[0].textContent = category[product].price;
+    learnMore.children[2].children[1].textContent = category[product].desc;
+    learnMore.children[2].children[2].children[0].innerHTML = category[product].caract1;
+    learnMore.children[2].children[2].children[1].innerHTML = category[product].caract2;
+    learnMore.children[2].children[3].innerHTML = category[product].advice;
+}
+
+learnMoreButton.forEach(button => {
+    button.addEventListener('click', () => {
+        learnMore.style.display = "flex";
+        if (button.classList[1] == "miels") {
+            createProductPage(honeys, button.classList[2]);
+        }
+        if (button.classList[1] == "bougies"){
+            createProductPage(candles, button.classList[2]);
+        }
+    })
+    
+})
+
+// document.querySelector(`.learnMore-button-miels0`).addEventListener('click',() => {
+//     console.log('run');
+//     learnMore.style.display = "flex";
+//     createProductPage(amount, i);
+// })
